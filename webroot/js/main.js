40b6e4eb1f436cdc0b3611779db993d744f54647
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-12 05:35:49 -0300

    conflict

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -18,26 +18,17 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-<<<<<<< HEAD
         url: '/jquery-file-upload/upload-handler'
     });
 
     // Enable iframe cross-domain access via redirect option:
     /*$('#fileupload').fileupload(
-=======
-        url: 'server/php/'
-    });
-
-    // Enable iframe cross-domain access via redirect option:
-    $('#fileupload').fileupload(
->>>>>>> 662e78340800a750e288549b5f0f506e10bcff0a
         'option',
         'redirect',
         window.location.href.replace(
             /\/[^\/]*$/,
             '/cors/result.html?%s'
         )
-<<<<<<< HEAD
     );*/
 
     // Load existing files:
@@ -54,49 +45,5 @@ $(function () {
         $(this).fileupload('option', 'done')
             .call(this, $.Event('done'), {result: result});
     });
-=======
-    );
-
-    if (window.location.hostname === 'blueimp.github.io') {
-        // Demo settings:
-        $('#fileupload').fileupload('option', {
-            url: '//jquery-file-upload.appspot.com/',
-            // Enable image resizing, except for Android and Opera,
-            // which actually support image resizing, but fail to
-            // send Blob objects via XHR requests:
-            disableImageResize: /Android(?!.*Chrome)|Opera/
-                .test(window.navigator.userAgent),
-            maxFileSize: 999000,
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-        });
-        // Upload server status check for browsers with CORS support:
-        if ($.support.cors) {
-            $.ajax({
-                url: '//jquery-file-upload.appspot.com/',
-                type: 'HEAD'
-            }).fail(function () {
-                $('<div class="alert alert-danger"/>')
-                    .text('Upload server currently unavailable - ' +
-                            new Date())
-                    .appendTo('#fileupload');
-            });
-        }
-    } else {
-        // Load existing files:
-        $('#fileupload').addClass('fileupload-processing');
-        $.ajax({
-            // Uncomment the following to send cross-domain cookies:
-            //xhrFields: {withCredentials: true},
-            url: $('#fileupload').fileupload('option', 'url'),
-            dataType: 'json',
-            context: $('#fileupload')[0]
-        }).always(function () {
-            $(this).removeClass('fileupload-processing');
-        }).done(function (result) {
-            $(this).fileupload('option', 'done')
-                .call(this, $.Event('done'), {result: result});
-        });
-    }
->>>>>>> 662e78340800a750e288549b5f0f506e10bcff0a
 
 });
